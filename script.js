52b557d1b6db07d5f5996006e81666e46307c741
MOATASSEM BILLAH SINDHU <moatassemsindhu@gmail.com>
2021-12-25 17:48:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineering Student", "Developer", "Blogger", "Freelancer"],
+        strings: ["Engineering Student", "Developer", "Blogger", "Freelancer","Speaker"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Engineering Student", "Developer", "Blogger", "Freelancer"],
+        strings: ["Engineering Student", "Developer", "Blogger", "Freelancer","Speaker"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
