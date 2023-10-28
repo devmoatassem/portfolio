fc14e37cf5ce4eddf25114604c715a4becefbad8
devmoatassem <moatassemsindhu@gmail.com>
2023-10-28 23:54:40 +0500

    Map function in nav list

--- new-portfolio/src/components/Navbar.jsx
+++ new-portfolio/src/components/Navbar.jsx
@@ -4,7 +4,35 @@ import "../assets/css/nav-bar.css"
 
 function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const navList = [
+    {
+      name: 'Home',
+      linkTo: '/home'
+    },
+    {
+      name: 'About',
+      linkTo: '/about'
+    },
+    {
+      name: 'Portfolio',
+      linkTo: '/portfolio'
+    },
+    {
+      name: 'Education',
+      linkTo: '/education'
+    }
+  ]
 
+  const rendered_nav_list = navList.map((navItem)=>(
+    <li key={navItem.name}>
+              <Link
+                to={navItem.linkTo}
+                className="block py-2 pl-3 pr-4 rounded active:bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white hover:text-white">
+                {navItem.name}
+              </Link>
+            </li>
+  ))
+  
   return (
     <nav className="bg-transparent w-full header top-0 left-0 z-50 h-auto absolute">
       <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4 justify-between">
@@ -14,10 +42,10 @@ function Navbar() {
           </Link>
         </div>
         <div className="flex md:order-2">
-          <Link className="container glass-effect inline-flex items-end fill-white hover:fill-blue-700 text-white hover:text-blue-700 font-medium rounded-lg px-3 py-2 md:px-8 md:py-3 text-center mr-1 md:mr-0 ">
-            
-            Hire Me<svg className="pl-2 mb-1 -mr-1"  height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
-             
+          <Link className="container glass-effect inline-flex items-end fill-white hover:fill-blue-700 text-white hover:text-blue-700 font-medium rounded-lg px-3 py-2 md:px-7 md:py-3 text-center mr-1 md:mr-0 ">
+
+            Hire Me<svg className="pl-2 mb-1 -mr-1" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
+
           </Link>
           <button
 
@@ -26,45 +54,13 @@ function Navbar() {
             onClick={() => setNavbarOpen(!navbarOpen)}
           >
             <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15"/>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
         </div>
         <div className={(navbarOpen ? "hidden" : "") + "hidden  items-center justify-between w-full md:flex md:w-96 md:order-1 md:mr-32 "}>
           <ul className="glass-effect flex flex-col p-4 md:p-0 mt-4 font-medium border  md:flex-row md:space-x-8 md:mt-0 md:border-0 md:place-content-center md: justify-between md:px-8 md:py-3 ">
-            <li>
-              <Link
-                to="/home"
-                className="block py-2 pl-3 pr-4 rounded active:bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white hover:text-white"
-              
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                className="block py-2 pl-3 pr-4 rounded active:bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white hover:text-white"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/portfolio"
-                className="block py-2 pl-3 pr-4 rounded active:bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white hover:text-white"
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/education"
-                className="block py-2 pl-3 pr-4 rounded active:bg-blue-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white hover:text-white"
-              >
-                Education
-              </Link>
-            </li>
+            {rendered_nav_list}
           </ul>
         </div>
       </div>
