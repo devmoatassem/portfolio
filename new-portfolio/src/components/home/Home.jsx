2de653ebfa9a4d8adffb70d2f1c84a9a2b921ea3
devmoatassem <moatassemsindhu@gmail.com>
2023-10-30 21:03:44 +0500

    Portfolio added to home

--- new-portfolio/src/components/home/Home.jsx
+++ new-portfolio/src/components/home/Home.jsx
@@ -1,8 +1,10 @@
 import About from "../about/About"
+import Portfolio from "../portfolio/Portfolio"
 function Home(){
     return(
         <>
         <About />
+        <Portfolio />
         </>
     )
 }
