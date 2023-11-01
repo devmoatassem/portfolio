3b824e33ca1c691784f491c273fead6ce716cf0d
devmoatassem <moatassemsindhu@gmail.com>
2023-11-01 23:33:07 +0500

    Hire button margin

--- new-portfolio/src/components/common/navbar/Navbar.jsx
+++ new-portfolio/src/components/common/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import "../../../assets/css/nav-bar.css"
 
 function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
-  
+
   return (
     <nav className="bg-transparent w-full header top-0 left-0 z-50 h-auto absolute">
       <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4 justify-between">
@@ -17,7 +17,7 @@ function Navbar() {
         <div className="flex md:order-2">
           <Link className="container rounded-lg border shadow-lg border-gray-600 bg-gray-700  gap-3 inline-flex items-end fill-white hover:fill-gray-950 text-white hover:text-gray-950 font-medium px-3 py-2 md:px-7 md:py-2.5 text-center mr-1 md:mr-0 ">
 
-            Hire Me<svg className=" mb-1 -mr-3.5 md:-mr-1" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
+            Hire Me<svg className="m-auto" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
 
           </Link>
           <button
@@ -33,7 +33,7 @@ function Navbar() {
         </div>
         <div className={(navbarOpen ? "hidden" : "") + "hidden  rounded-lg border shadow-lg border-gray-600 bg-gray-800 items-center justify-between w-full md:flex md:w-96 md:order-1 md:mr-32 "}>
           <ul className=" flex flex-col space-y-4 md:space-y-0 p-4 md:p-0 mt-4 font-medium md:flex-row md:space-x-8 md:mt-0 md:border-0 md:place-content-center md: justify-between md:px-8 md:py-3 ">
-              <NavLinks/>
+            <NavLinks />
           </ul>
         </div>
       </div>
