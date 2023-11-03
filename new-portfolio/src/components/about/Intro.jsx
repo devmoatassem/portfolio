5779d240a2c56d4543d3eb045c3ae16f2f4abdec
devmoatassem <moatassemsindhu@gmail.com>
2023-11-03 22:36:43 +0500

    intro section animation done

--- new-portfolio/src/components/about/Intro.jsx
+++ new-portfolio/src/components/about/Intro.jsx
@@ -1,24 +1,51 @@
 import Scroll from "./Scroll";
 import Socials from '../common/Socials'
+import { motion } from "framer-motion"
+import { introVariants } from "../common/animationVarients";
 function Intro() {
   return (
-    <div className="px-4 w-full py-20 text-center md:w-3/4">
-      <div className="relative mb-5 inline-block overflow-hidden rounded-full align-middle">
-        <div>
-          <img
-            className=""
-            src="https://media.licdn.com/dms/image/D4D03AQGgAJd7HnYLpg/profile-displayphoto-shrink_200_200/0/1672036507003?e=1703721600&v=beta&t=JvLzOxNLvDuh0tC90-dBOmIUotOP4VH6NcyQQHCV8Ms"
-            alt="Moatassem"
-          />
-        </div>
-      </div>
+    <div
+      className="px-4 w-full py-20 text-center md:w-3/4">
+      <motion.div
+        variants={introVariants}
+        initial="hidden"
+        transition={{ delay: 0.2, duration: 0.4 }}
+        viewport={{ once: true }}
+        whileInView="visible"
+        className="relative mb-5 inline-block overflow-hidden rounded-full align-middle">
+        <img
+          className=""
+          src="https://media.licdn.com/dms/image/D4D03AQGgAJd7HnYLpg/profile-displayphoto-shrink_200_200/0/1672036507003?e=1703721600&v=beta&t=JvLzOxNLvDuh0tC90-dBOmIUotOP4VH6NcyQQHCV8Ms"
+          alt="Moatassem"
+        />
+      </motion.div>
 
-      <h1 className="text-white mb-4 text-4xl font-extrabold">Hi, I am Moatassem Billah</h1>
+      <motion.h1
+        variants={introVariants}
+        initial="hidden"
+        transition={{ delay: 0.4, duration: 0.4 }}
+        viewport={{ once: true }}
+        whileInView="visible"
+        className="text-white mb-4 text-4xl font-extrabold">Hi, I am Moatassem Billah</motion.h1>
 
-      <p className="text-gray-400 mb-0 font-semibold text-xl">MERN | Python | Flask Dev | AI Enthusiast | Mentor@MLSA | SSR IEEE Faisalabad Sub Section | Former Google DSC Lead'22 | β Microsoft Learn Student Ambassador</p>
-      <div className="mt-7 text-center" >
+      <motion.p
+        variants={introVariants}
+        initial="hidden"
+        transition={{ delay: 0.6, duration: 0.4 }}
+        viewport={{ once: true }}
+        whileInView="visible"
+        className="text-gray-400 mb-0 font-semibold text-xl">
+        MERN | Python | Flask Dev | AI Enthusiast | Mentor@MLSA | SSR IEEE Faisalabad Sub Section | Former Google DSC Lead'22 | β Microsoft Learn Student Ambassador
+      </motion.p>
+      <motion.div
+        variants={introVariants}
+        initial="hidden"
+        transition={{ delay: 0.8, duration: 0.4 }}
+        viewport={{ once: true }}
+        whileInView="visible"
+        className="mt-7 text-center" >
         <Socials />
-      </div>
+      </motion.div>
       <Scroll />
     </div>
   );
--- new-portfolio/src/components/about/Scroll.jsx
+++ new-portfolio/src/components/about/Scroll.jsx
@@ -1,28 +1,36 @@
-function Scroll(){
-    return(
-        <div
-  className="fill-white text-white absolute left-0 top-auto bottom-10 w-full justify-between text-center"
-  style={{ opacity: 1, transform: "none" }}>
-  <a className=" cursor-pointer text-xs font-medium uppercase tracking-widest transition-all hover:text-primary">
-    <svg
-      
-      strokeWidth={0}
-      viewBox="0 0 24 24"
-      className=" inline animate-bounce text-base"
-      height="1em"
-      width="1em"
-      
-    >
-      <g>
-        <path fill="none" d="M0 0h24v24H0z" />
-        <path d="M13 16.172l5.364-5.364 1.414 1.414L12 20l-7.778-7.778 1.414-1.414L11 16.172V4h2v12.172z" />
-      </g>
-    </svg>
-    <span className="pl-2">Scroll Down</span>
-  </a>
-</div>
+import { motion } from "framer-motion";
+import { introVariants } from "../common/animationVarients";
 
-    )
+function Scroll() {
+  return (
+    <motion.div
+      variants={introVariants}
+      initial="hidden"
+      transition={{ delay: 1, duration: 0.4 }}
+      viewport={{ once: true }}
+      whileInView="visible"
+      className="fill-white text-white absolute left-0 top-auto bottom-10 w-full justify-between text-center"
+      style={{ opacity: 1, transform: "none" }}>
+      <a className=" cursor-pointer text-xs font-medium uppercase tracking-widest transition-all hover:text-primary">
+        <svg
+
+          strokeWidth={0}
+          viewBox="0 0 24 24"
+          className=" inline animate-bounce text-base"
+          height="1em"
+          width="1em"
+
+        >
+          <g>
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M13 16.172l5.364-5.364 1.414 1.414L12 20l-7.778-7.778 1.414-1.414L11 16.172V4h2v12.172z" />
+          </g>
+        </svg>
+        <span className="pl-2">Scroll Down</span>
+      </a>
+    </motion.div>
+
+  )
 }
 
 export default Scroll;
