69e73be8ceb0b6c4aa647bc1a21b20e71a0eb089
devmoatassem <moatassemsindhu@gmail.com>
2023-11-03 23:08:40 +0500

    Heading part animation

--- new-portfolio/src/components/common/HeaderC.jsx
+++ new-portfolio/src/components/common/HeaderC.jsx
@@ -1,9 +1,16 @@
-
-function HeaderC({topPadding ='mt-14', ...props}) {
+import { motion } from "framer-motion";
+import { introVariants } from "./animationVarients";
+function HeaderC({ topPadding = 'mt-14', ...props }) {
     return (
         <section className="relative">
             <div className="main-bg absolute py left-0 top-0 h-full w-full"></div>
-            <div className="max-w-screen-xl container  mx-auto">
+            <motion.div
+                variants={introVariants}
+                initial="hidden"
+                transition={{ delay: 0.5, duration: 0.4 }}
+                viewport={{ once: true }}
+                whileInView="visible"
+                className="max-w-screen-xl container  mx-auto">
                 <div className="flex w-full items-center justify-center">
                     <div className={topPadding}>
                         <div className="py-16 px-4 mx-auto max-w-screen-md text-center">
@@ -12,10 +19,9 @@ function HeaderC({topPadding ='mt-14', ...props}) {
                         </div>
                     </div>
                 </div>
-            </div>
+            </motion.div>
             <hr className="md:mx-auto border-gray-700" />
         </section>
     )
 }
-
 export default HeaderC;
