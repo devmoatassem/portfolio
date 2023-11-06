af029d866b7057b8cfa27d5693d3a0477cea2d4a
devmoatassem <moatassemsindhu@gmail.com>
2023-11-06 08:05:59 +0500

    new portfolio in main

---
+++ old-portfolio/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        //strings: ["Speaker", "Developer", "Blogger","Pythonista", "Freelancer","EE Student","IEEEian"],
+        strings: ["Techie","Pythonista", "Freelancer","EE Student","IEEEian","GDSC Lead'22","MLSA"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    /*var typed = new Typed(".typing-2", {
+        strings: ["Speaker", "Developer", "Blogger", "Freelancer","Engineering Student","IEEEian"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    }); */
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
