6c37cd7aa516a9ad12ef41f7998183a55fa7d834
MOATASSEM BILLAH SANDHU <moatassemsindhu@gmail.com>
2025-05-17 17:21:05 +0500

    hero

--- moatassem-portfolio/src/Header/Component.client.tsx
+++ moatassem-portfolio/src/Header/Component.client.tsx
@@ -30,7 +30,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   }, [headerTheme])
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
+    <header className="container relative z-50 " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
         <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
--- moatassem-portfolio/src/heros/HighImpact/index.tsx
+++ moatassem-portfolio/src/heros/HighImpact/index.tsx
@@ -7,6 +7,8 @@ import type { Page } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
+import { GridBackground, Spotlight } from '@/components/ui/spotlight'
+import { BackgroundCells } from '@/components/ui/background-ripple'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   const { setHeaderTheme } = useHeaderTheme()
@@ -17,30 +19,35 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
 
   return (
     <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
+      className="relative overflow-hidden -mt-[10.4rem] text-white bg-black/[0.96]"
       data-theme="dark"
     >
-      <div className="container mb-8 z-10 relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] md:text-center">
-          {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
-          {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex md:justify-center gap-4">
-              {links.map(({ link }, i) => {
-                return (
-                  <li key={i}>
-                    <CMSLink {...link} />
-                  </li>
-                )
-              })}
-            </ul>
-          )}
+      {/* <GridBackground /> */}
+      <Spotlight />
+      <BackgroundCells >
+        <div className="container mb-8 z-10 relative flex items-center justify-center">
+          <div className="max-w-[36.5rem] md:text-center">
+            {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+            {Array.isArray(links) && links.length > 0 && (
+              <ul className="flex md:justify-center gap-4">
+                {links.map(({ link }, i) => {
+                  return (
+                    <li key={i}>
+                      <CMSLink {...link} />
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="min-h-[80vh] select-none">
+     
+      {/* <div className="min-h-[90vh] select-none">
         {media && typeof media === 'object' && (
           <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
         )}
-      </div>
+        </div> */}
+        </BackgroundCells>
     </div>
   )
 }
