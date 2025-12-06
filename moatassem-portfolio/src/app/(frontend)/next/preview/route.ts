7782ecf444a29497edb7daed97438e7f90993308
devmoatassem <moatassemsindhu@gmail.com>
2025-12-07 01:30:48 +0500

    chore: update Payload CMS dependencies to version 3.67.0 for improved stability and features

--- moatassem-portfolio/src/app/(frontend)/next/preview/route.ts
+++ moatassem-portfolio/src/app/(frontend)/next/preview/route.ts
@@ -3,21 +3,14 @@ import { getPayload } from 'payload'
 
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
+import type { NextRequest } from 'next/server'
 
 import configPromise from '@payload-config'
 
-export async function GET(
-  req: {
-    cookies: {
-      get: (name: string) => {
-        value: string
-      }
-    }
-  } & Request,
-): Promise<Response> {
+export async function GET(request: NextRequest): Promise<Response> {
   const payload = await getPayload({ config: configPromise })
 
-  const { searchParams } = new URL(req.url)
+  const { searchParams } = new URL(request.url)
 
   const path = searchParams.get('path')
   const collection = searchParams.get('collection') as CollectionSlug
@@ -40,8 +33,8 @@ export async function GET(
 
   try {
     user = await payload.auth({
-      req: req as unknown as PayloadRequest,
-      headers: req.headers,
+      req: request as unknown as PayloadRequest,
+      headers: request.headers,
     })
   } catch (error) {
     payload.logger.error({ err: error }, 'Error verifying token for live preview')
