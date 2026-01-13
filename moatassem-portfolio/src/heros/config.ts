361749a4bd1186a81e8896d9252a6985b847b1fd
devmoatassem <moatassemsindhu@gmail.com>
2026-01-13 18:01:20 +0500

    fix: update media upload condition to only allow mediumImpact type

--- moatassem-portfolio/src/heros/config.ts
+++ moatassem-portfolio/src/heros/config.ts
@@ -62,7 +62,7 @@ export const hero: Field = {
       name: 'media',
       type: 'upload',
       admin: {
-        condition: (_, { type } = {}) => ['highImpact', 'mediumImpact'].includes(type),
+        condition: (_, { type } = {}) => ['mediumImpact'].includes(type),
       },
       relationTo: 'media',
       required: true,
