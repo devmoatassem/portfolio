03f53dc9ed8fdfcfe84983f1b2a9d9408e47c7d8
devmoatassem <moatassemsindhu@gmail.com>
2026-01-28 20:37:07 +0500

    refactor: remove unused authors and populatedAuthors fields from Projects collection

--- moatassem-portfolio/src/collections/Projects/index.ts
+++ moatassem-portfolio/src/collections/Projects/index.ts
@@ -231,39 +231,6 @@ export const Projects: CollectionConfig<'projects'> = {
         ],
       },
     },
-    {
-      name: 'authors',
-      type: 'relationship',
-      admin: {
-        position: 'sidebar',
-      },
-      hasMany: true,
-      relationTo: 'users',
-    },
-    // This field is only used to populate the user data via the `populateAuthors` hook
-    // This is because the `user` collection has access control locked to protect user privacy
-    // GraphQL will also not return mutated user data that differs from the underlying schema
-    {
-      name: 'populatedAuthors',
-      type: 'array',
-      access: {
-        update: () => false,
-      },
-      admin: {
-        disabled: true,
-        readOnly: true,
-      },
-      fields: [
-        {
-          name: 'id',
-          type: 'text',
-        },
-        {
-          name: 'name',
-          type: 'text',
-        },
-      ],
-    },
     ...slugField(),
   ],
   hooks: {
