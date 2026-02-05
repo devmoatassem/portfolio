55aa226ea46dd7bc32f883fa5ee1ad5845facdc2
devmoatassem <moatassemsindhu@gmail.com>
2026-02-05 20:16:54 +0500

    fix: double title name

--- moatassem-portfolio/src/utilities/generateMeta.ts
+++ moatassem-portfolio/src/utilities/generateMeta.ts
@@ -26,7 +26,7 @@ export const generateMeta = async (args: {
 
   const ogImage = getImageURL(doc?.meta?.image)
 
-  const title = doc?.meta?.title ? doc?.meta?.title + ' | Moatassem' : 'Moatassem'
+  const title = doc?.meta?.title ? doc?.meta?.title : 'Moatassem'
 
   return {
     description: doc?.meta?.description,
