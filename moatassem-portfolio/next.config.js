83b998d21bc847e52362cb6cd5df52441211e034
devmoatassem <moatassemsindhu@gmail.com>
2026-05-07 00:23:01 +0500

    Update next.config.js

--- moatassem-portfolio/next.config.js
+++ moatassem-portfolio/next.config.js
@@ -8,6 +8,9 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    inlineCss: true,
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
