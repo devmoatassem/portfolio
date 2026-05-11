ae82c25718437b01d57d1bc71654380f709edeaf
devmoatassem <moatassemsindhu@gmail.com>
2026-05-11 22:31:33 +0500

    fix: adjust positioning and padding in Timeline component for improved layout

--- moatassem-portfolio/src/components/ui/timline.tsx
+++ moatassem-portfolio/src/components/ui/timline.tsx
@@ -42,7 +42,7 @@ export const Timeline = ({ data }: { data: NonNullable<Timline['events']> }) => 
         {data.map((item, index) => (
           <div key={index} className="flex justify-start pt-10 md:pt-40 md:gap-10">
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-background  flex items-center justify-center">
+              <div className="h-10 absolute -left-3.5 md:left-3 w-10 rounded-full bg-background  flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-muted-foreground border border-border p-2" />
               </div>
               <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-muted-foreground">
@@ -50,7 +50,7 @@ export const Timeline = ({ data }: { data: NonNullable<Timline['events']> }) => 
               </h3>
             </div>
 
-            <div className="relative pl-20 pr-4 md:pl-4 w-full overflow-hidden">
+            <div className="relative pl-10 pr-4 md:pl-4 w-full overflow-hidden">
               <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-muted-foreground">
                 {item.title}
               </h3>
@@ -62,7 +62,7 @@ export const Timeline = ({ data }: { data: NonNullable<Timline['events']> }) => 
           style={{
             height: height + 'px',
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute left-1 md:left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
